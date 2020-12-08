bf236b8a917a2773e95b58c88b6dfff74c1e25c4
Marie Otaki <otakimarie@gmail.com>
2020-12-07 16:00:00 -0800

    add a mock server

---
+++ src/mocks/handlers.js
@@ -0,0 +1,23 @@
+import { rest } from 'msw';
+
+const baseURL = 'https://www.googleapis.com/youtube/v3';
+
+const KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
+
+const commonParams = {
+  part: 'snippet',
+  maxResults: '40',
+  key: KEY,
+  regionCode: 'CA',
+  type: 'video',
+};
+
+export const handlers = [
+  rest.get(`${baseURL}/videos`, (req, res, ctx) => {
+    const params = {
+      ...commonParams,
+      chart: 'mostPopular',
+    };
+    return res(ctx.status(200), ctx.json({ params, test: 'dammy' }));
+  }),
+];
