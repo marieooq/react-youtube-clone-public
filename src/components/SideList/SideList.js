7d8fa25b949f033bbec4d109bc7cf285d201dde0
Marie Otaki <otakimarie@gmail.com>
2020-12-11 16:57:32 -0800

    add the condition to check if each object has snippet

--- src/components/SideList/SideList.js
+++ src/components/SideList/SideList.js
@@ -9,14 +9,16 @@ const SideList = () => {
     <div className={Style.sidenav}>
       {globalState.related ? (
         globalState.related.map((video) => {
-          return (
-            <SideListItem
-              id={video.id.videoId}
-              key={video.id.videoId}
-              src={video.snippet.thumbnails.medium.url}
-              title={video.snippet.title}
-            />
-          );
+          if (video.snippet) {
+            return (
+              <SideListItem
+                id={video.id.videoId}
+                key={video.id.videoId}
+                src={video.snippet.thumbnails.medium.url}
+                title={video.snippet.title}
+              />
+            );
+          }
         })
       ) : (
         <span>no data</span>
